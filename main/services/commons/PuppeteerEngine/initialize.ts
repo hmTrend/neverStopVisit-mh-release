ae5d65fcda1aac209decc8a2e9f72bf93415b6d7
applepykim <applepykim@naver.com>
2024-11-10 00:04:16 +0900

    v0.3.1 + 모바일 창으로 열기

--- main/services/commons/PuppeteerEngine/initialize.ts
+++ main/services/commons/PuppeteerEngine/initialize.ts
@@ -2,5 +2,10 @@ export const initialize = async ({ url, browser, page, puppeteer, pages }) => {
   browser = await puppeteer.launch({ headless: false });
   pages = await browser.pages();
   page = pages[0];
+  await page.setViewport({ width: 375, height: 812 }); // Specifying the viewport of iPhone X manually
+  await page.setUserAgent(
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
+  );
+
   await page.goto(url);
 };
