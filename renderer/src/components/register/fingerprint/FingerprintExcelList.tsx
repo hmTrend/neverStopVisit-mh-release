e3d06c235c3f067b19457bb3e5cc119b606c0a45
applepykim <applepykim@naver.com>
2024-11-11 19:46:03 +0900

    v0.7.1 + 지문 > 리스트 2

--- renderer/src/components/register/fingerprint/FingerprintExcelList.tsx
+++ renderer/src/components/register/fingerprint/FingerprintExcelList.tsx
@@ -28,14 +28,13 @@ export const FingerprintExcelList = () => {
             <Kbd fontSize={"md"}>{selectedGroupName}</Kbd> 지문 리스트
           </FormLabel>
           <TableContainer>
-            <Table variant="striped" colorScheme="teal" size={"sm"}>
+            <Table variant="striped" colorScheme="gray" size={"sm"}>
               <TableCaption>선택된 지문 리스트</TableCaption>
               <Thead>
                 <Tr>
                   <Th>3개</Th>
                   <Th>쿠키</Th>
                   <Th>만든날짜</Th>
-                  <Th>개별열기</Th>
                 </Tr>
               </Thead>
               <Tbody>
@@ -43,25 +42,16 @@ export const FingerprintExcelList = () => {
                   <Td>1</Td>
                   <Td>[cookie list]</Td>
                   <Td isNumeric>2024-10-22</Td>
-                  <Td>
-                    <Button variant={"ghost"}>오픈</Button>
-                  </Td>
                 </Tr>
                 <Tr>
                   <Td>2</Td>
                   <Td>[cookie list]</Td>
                   <Td isNumeric>2024-10-22</Td>
-                  <Td>
-                    <Button variant={"ghost"}>오픈</Button>
-                  </Td>
                 </Tr>
                 <Tr>
                   <Td>3</Td>
                   <Td>[cookie list]</Td>
                   <Td isNumeric>2024-10-22</Td>
-                  <Td>
-                    <Button variant={"ghost"}>오픈</Button>
-                  </Td>
                 </Tr>
               </Tbody>
             </Table>
