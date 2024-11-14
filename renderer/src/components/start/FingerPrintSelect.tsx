4d35a37bb948cf84c521ee3c83ed7ac770495e57
applepykim <applepykim@naver.com>
2024-11-14 15:53:07 +0900

    v0.20.1 + 시작 > 지문 설정 완료

--- renderer/src/components/start/FingerPrintSelect.tsx
+++ renderer/src/components/start/FingerPrintSelect.tsx
@@ -1,13 +1,21 @@
 import { Box, Flex, Select, Text } from "@chakra-ui/react";
 import { useSnapshot } from "valtio/react";
 import { storeFingerPrintRegister } from "@/valtio/fingerPrint.register.valtio";
+import { storeStart } from "@/valtio/start.valtio";
 
 export const FingerPrintSelect = ({ selectProgram }) => {
   const { groupList } = useSnapshot(storeFingerPrintRegister);
 
   const handleSelectChange = (e, programTitle) => {
-    const selectedValue = e.target.value;
-    const storageKey = `selectedFingerPrint_${programTitle}`;
+    const groupId = e.target.value;
+    const selectedOption = e.target.options[e.target.selectedIndex];
+    const groupName = selectedOption.text; // 선택된 option의 텍스트(groupName) 가져오기
+
+    console.log("groupId");
+    console.log(groupId);
+    console.log("groupName");
+    console.log(groupName);
+    storeStart.nShopping.fingerPrint = { groupId, groupName };
   };
 
   return (
--- renderer/src/valtio/start.valtio.ts
+++ renderer/src/valtio/start.valtio.ts
@@ -3,6 +3,7 @@ import { proxy } from "valtio/vanilla";
 class StartValtio {
   common: { memberFid: string; networkConnectType: string };
   nShopping: {
+    isStart: boolean;
     selectedGroup: { groupName: string; groupId: string };
     fingerPrint: { groupName: string; groupId: string };
     concurrentBrowserCount: number;
@@ -11,6 +12,7 @@ class StartValtio {
   constructor() {
     this.common = {};
     this.nShopping = {
+      isStart: false,
       selectedGroup: {
         groupName: "",
         groupId: "",
