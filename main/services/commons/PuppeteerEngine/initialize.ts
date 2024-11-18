19ed809c8a6a9016f46735c6623f371e312d5777
applepykim <applepykim@naver.com>
2024-11-18 21:14:29 +0900

    NSV-15 쇼핑 로직 완성하기 + userAgent 임시추가 + 모바일 viewport 추가

--- main/services/commons/PuppeteerEngine/initialize.ts
+++ main/services/commons/PuppeteerEngine/initialize.ts
@@ -18,11 +18,18 @@ export const initialize = async ({
   cookie;
 }) => {
   const proxySettings = getNextProxy();
+  const userAgent =
+    "Mozilla/5.0 (Linux; Android 13; SM-S911N Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/102.0.5005.189 Whale/1.0.0.0 Crosswalk/27.102.0.18 Mobile Safari/537.36)/4.0 Chrome/84.0.4368.53 Mobile Safari/537.36 Maxthon/3248";
   browser = await chromiumEngine.launch({
     headless: false,
-    proxy: { server: proxySettings },
+    // proxy: { server: proxySettings },
   });
-  const context = await browser.newContext(devices["iPhone 15 Pro Max"]);
+  const context = await browser.newContext({
+    userAgent: userAgent,
+    viewport: { width: 412, height: 915 },
+    isMobile: true,
+    hasTouch: true,
+  });
   if (cookie && cookie.length > 0) {
     const formattedCookies = formatCookiesForPlaywright(cookie);
     await context.addCookies(formattedCookies);
