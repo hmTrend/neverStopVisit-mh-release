4d23cb34b70891f9fd05aeeef7a212bda00fa7fb
applepykim <applepykim@naver.com>
2024-11-21 18:56:45 +0900

    v1.1.2 가격비교 버튼 클릭요소 추가

--- main/services/nShopping/plusStoreToComparePricing.ts
+++ main/services/nShopping/plusStoreToComparePricing.ts
@@ -3,13 +3,19 @@ import wait from "waait";
 
 export const plusStoreToComparePricing = async ({ page }: { page: Page }) => {
   try {
-    const shoppingButton = page.locator(
-      'a[data-shp-contents-dtl*="가격비교 검색결과 전환"]',
-    );
-
-    // 요소 대기 및 클릭
-    await shoppingButton.waitFor({ state: "visible" });
-    await shoppingButton.click();
+    try {
+      const shoppingButton = page.locator(
+        'a[data-shp-contents-dtl*="가격비교 검색결과 전환"]',
+      );
+      await shoppingButton.waitFor({ state: "visible" });
+      await shoppingButton.click();
+    } catch (e) {
+      const elementLocator = page.locator(
+        "/html/body/div/div[2]/div/div[1]/div/div[1]/div[2]/div/div[2]/ul/li[2]/div/a",
+      );
+      await elementLocator.waitFor({ state: "visible", timeout: 10000 });
+      await elementLocator.click();
+    }
     await page.waitForLoadState("load");
     await wait(1500);
   } catch (e) {
