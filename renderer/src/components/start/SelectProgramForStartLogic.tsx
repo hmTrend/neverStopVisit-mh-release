fcae2f38c1419ebcaee5c64d0d11cf5201b9fa9c
applepykim <applepykim@naver.com>
2024-11-29 09:44:50 +0900

    nPlace 시작 UI 만드는중..

--- renderer/src/components/start/SelectProgramForStartLogic.tsx
+++ renderer/src/components/start/SelectProgramForStartLogic.tsx
@@ -4,6 +4,7 @@ import { SelectProgramForStart } from "@/components/start/SelectProgramForStart"
 import { useSnapshot } from "valtio/react";
 import { storeNShopping } from "@/valtio/nShopping.register.valtio";
 import { storeStart } from "@/valtio/start.valtio";
+import { Flex } from "@chakra-ui/react";
 
 export const SelectProgramForStartLogic = () => {
   const { groupList: nShoppingGroupList } = useSnapshot(storeNShopping);
@@ -27,11 +28,37 @@ export const SelectProgramForStartLogic = () => {
     }
   };
 
+  const handleSelectChangeNPlace = (e) => {
+    const selectedId = e.target.value;
+    // groupList에서 선택된 그룹 찾기
+    const selectedGroup: any = nShoppingGroupList.find(
+      (group: any) => group._id === selectedId,
+    );
+
+    if (selectedGroup) {
+      const newSelectedGroup = {
+        groupName: selectedGroup.groupName,
+        groupId: selectedGroup._id,
+      };
+
+      // valtio store 업데이트
+      storeStart.nShopping.selectedGroup = newSelectedGroup;
+      // localStorage 업데이트
+    }
+  };
+
   return (
-    <SelectProgramForStart
-      groupList={nShoppingGroupList}
-      handleSelectChange={handleSelectChangeNShopping}
-      selectProgram={"nShopping"}
-    />
+    <Flex direction={"column"} gap={9}>
+      <SelectProgramForStart
+        groupList={nShoppingGroupList}
+        handleSelectChange={handleSelectChangeNShopping}
+        selectProgram={"nShopping"}
+      />
+      <SelectProgramForStart
+        groupList={nShoppingGroupList}
+        handleSelectChange={handleSelectChangeNPlace}
+        selectProgram={"nPlace"}
+      />
+    </Flex>
   );
 };
