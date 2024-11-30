7c6e3835f8ea65d922a0b9a312537d831437413a
applepykim <applepykim@naver.com>
2024-12-01 06:01:50 +0900

    findTargetBlog

---
+++ main/services/nPlace/findTargetBlog.ts
@@ -0,0 +1,43 @@
+import { Page } from "playwright";
+import { PuppeteerEngine } from "../commons/PuppeteerEngine";
+
+export const findTargetBlog = async ({
+  page = undefined,
+  targetBlog = "강남역 신논현역 파스타 치킨 맛집 “치스타리에 강남역점”",
+  isTest = true,
+}: {
+  page?: Page;
+  targetBlog?: string;
+  isTest?: boolean;
+} = {}) => {
+  try {
+    if (isTest) {
+      const test = new PuppeteerEngine();
+      await test.initialize({
+        url: "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EA%B0%95%EB%82%A8%EB%A7%9B%EC%A7%91+%EC%B9%98%EC%8A%A4%ED%83%80%EB%A6%AC%EC%97%90+%EA%B0%95%EB%82%A8%EC%97%AD%EC%A0%90",
+        cookie: "",
+      });
+      page = test.page;
+    }
+
+    try {
+      const link = page.locator(`a:has-text("${targetBlog}")`);
+
+      // 링크가 있는지 확인하고 클릭
+      await link.waitFor({ state: "visible", timeout: 5000 });
+      await Promise.all([
+        page.waitForLoadState("networkidle"), // 네트워크 요청이 완료될 때까지 대기
+        await link.click(),
+      ]);
+    } catch (error) {
+      throw new Error(`"${targetBlog}" 링크를 찾을 수 없습니다.`);
+    }
+  } catch (e) {
+    console.error(e.message);
+    throw Error("ERR > targetKeywordSearch");
+  }
+
+  return { page };
+};
+
+findTargetBlog();
--- main/services/nPlace/targetKeywordSearch.ts
+++ main/services/nPlace/targetKeywordSearch.ts
@@ -14,23 +14,24 @@ export const targetKeywordSearch = async ({
     if (isTest) {
       const test = new PuppeteerEngine();
       await test.initialize({ url: "https://www.naver.com/", cookie: "" });
-      const searchInput = test.page.locator("#MM_SEARCH_FAKE");
+      page = test.page;
+    }
+    const searchInput = page.locator("#MM_SEARCH_FAKE");
 
-      // 요소가 존재하는지 확인하고 텍스트 입력
-      if (await searchInput.isVisible()) {
-        await searchInput.fill("강남맛집 치스타리에 강남역점");
-      } else {
-        console.log("검색 입력창을 찾을 수 없습니다.");
-      }
+    // 요소가 존재하는지 확인하고 텍스트 입력
+    if (await searchInput.isVisible()) {
+      await searchInput.fill("강남맛집 치스타리에 강남역점");
+    } else {
+      console.log("검색 입력창을 찾을 수 없습니다.");
+    }
 
-      const searchButton = test.page.locator("button.MM_SEARCH_SUBMIT");
-      try {
-        // 검색 버튼이 나타날 때까지 대기하고 클릭
-        await searchButton.waitFor({ state: "visible" });
-        await searchButton.click();
-      } catch (error) {
-        console.log("검색 버튼을 찾을 수 없습니다:", error);
-      }
+    const searchButton = page.locator("button.MM_SEARCH_SUBMIT");
+    try {
+      // 검색 버튼이 나타날 때까지 대기하고 클릭
+      await searchButton.waitFor({ state: "visible" });
+      await searchButton.click();
+    } catch (error) {
+      console.log("검색 버튼을 찾을 수 없습니다:", error);
     }
   } catch (e) {
     console.error(e.message);
