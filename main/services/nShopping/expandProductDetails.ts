ebb0042a63112d41019c0b054cdd2ace1e850439
applepykim <applepykim@naver.com>
2024-12-19 16:00:23 +0900

    NSV-11 / v2.8.8

--- main/services/nShopping/expandProductDetails.ts
+++ main/services/nShopping/expandProductDetails.ts
@@ -1,19 +1,22 @@
 import { Page } from "playwright";
 import wait from "waait";
-import { errorRetryBoundery } from "../commons/errorRetry";
 
 export const expandProductDetails = async ({ page }: { page: Page }) => {
+  let newPage: Page;
+
   try {
     await wait(1000);
-    const pages = page.context().pages();
-    page = pages[pages.length - 1]; // 가장 최근에 열린 탭
-    await page.keyboard.press("End");
+    const context = page.context();
+    const allPages = context.pages();
+    newPage = allPages[allPages.length - 1]; // 가장 최근에 열린 탭
+
+    await newPage.keyboard.press("End");
     await wait(2000);
-    await page.locator('button[data-shp-inventory="detailitm"]').click();
+    await newPage.locator('button[data-shp-inventory="detailitm"]').click();
     await wait(3000);
-    return { page };
+    return { page: newPage };
   } catch (e) {
     console.error(`detailitm not found > ${e.message}`);
-    return { page };
+    return { page: newPage }; // 이제 newPage에 접근 가능
   }
 };
