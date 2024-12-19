498b2b467646db0ad1ebae4ac6d7a98b93d0ff54
applepykim <applepykim@naver.com>
2024-12-19 14:00:56 +0900

    NSV-11 / goToShopping 이슈 처리

--- main/services/nShopping/goToShopping.ts
+++ main/services/nShopping/goToShopping.ts
@@ -2,11 +2,13 @@ import { Page } from "playwright";
 
 export const goToShopping = async ({ page }: { page: Page }) => {
   try {
-    await page.getByRole("link", { name: "스토어" }).first().click();
-    await page.waitForLoadState("load");
+    await Promise.all([
+      page.waitForLoadState("load"),
+      page.getByRole("link", { name: "스토어" }).first().click(),
+    ]);
     return { page };
   } catch (e) {
     console.error(e.message);
-    throw Error("goToShopping");
+    throw Error(`goToShopping > ${e.message}`);
   }
 };
