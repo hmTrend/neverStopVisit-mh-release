7ef300440c6edb84c84610702ac841bc0b10930d
applepykim <applepykim@naver.com>
2024-12-19 16:47:13 +0900

    NSV-11 / 쇼핑 더보기 클릭 로직 변경

--- main/services/nShopping/expandProductDetails.ts
+++ main/services/nShopping/expandProductDetails.ts
@@ -3,20 +3,23 @@ import wait from "waait";
 
 export const expandProductDetails = async ({ page }: { page: Page }) => {
   let newPage: Page;
-
-  try {
-    await wait(1000);
-    const context = page.context();
-    const allPages = context.pages();
-    newPage = allPages[allPages.length - 1]; // 가장 최근에 열린 탭
-    await wait(1000);
-    await newPage.keyboard.press("End");
-    await wait(2000);
-    await newPage.locator('button[data-shp-inventory="detailitm"]').click();
-    await wait(3000);
-    return { page: newPage };
-  } catch (e) {
-    console.error(`detailitm not found > ${e.message}`);
-    return { page: newPage }; // 이제 newPage에 접근 가능
+  for (let i = 0; i < 4; i++) {
+    try {
+      await wait(1000);
+      const context = page.context();
+      const allPages = context.pages();
+      newPage = allPages[allPages.length - 1]; // 가장 최근에 열린 탭
+      const button = newPage.locator('button[data-shp-inventory="detailitm"]');
+      await button.scrollIntoViewIfNeeded();
+      await wait(1000);
+      await button.click({ timeout: 3 * 1000 });
+      await wait(2000);
+      return { page: newPage };
+    } catch (e) {
+      console.error(`detailitm not found > ${e.message}`);
+      if (i === 3) {
+        return { page: newPage };
+      }
+    }
   }
 };
