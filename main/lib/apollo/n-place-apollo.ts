ea18dc7c4b01b4fa736a1c284fcf8430818a5d45
applepykim <applepykim@naver.com>
2025-01-23 09:11:17 +0900

    NSV-18 플레이스 타수 보기페이지 v3.4.0

--- main/lib/apollo/n-place-apollo.ts
+++ main/lib/apollo/n-place-apollo.ts
@@ -48,3 +48,57 @@ export const GetNPlaceExcelAlignFlatTargetOne = async ({ groupFid }) => {
     error: "",
   };
 };
+
+const gqlPatchNPlaceDayNowCount = gql`
+  mutation PatchNPlaceDayNowCount($input: PatchNPlaceDayNowCountInput!) {
+    patchNPlaceDayNowCount(input: $input) {
+      data {
+        _id
+        groupFid
+        keyword
+        delayTime
+        placeName
+        placeNumber
+        dayCount
+        dayNowCount
+        subKeywordList {
+          dayCount
+          targetBlog
+          targetKeyword
+        }
+        createdAt
+        updatedAt
+      }
+      message
+      error
+    }
+  }
+`;
+
+export const PatchNPlaceDayNowCount = async ({ groupFid, placeNumber }) => {
+  const { data, errors } = await client.mutate({
+    mutation: gqlPatchNPlaceDayNowCount,
+    variables: {
+      input: {
+        groupFid,
+        placeNumber,
+      },
+    },
+    fetchPolicy: "no-cache",
+  });
+  console.log("data 77777777");
+  console.log(data);
+  if (errors) {
+    console.error(errors[0].message);
+    return {
+      data,
+      message: "err > PatchNPlaceDayNowCount ",
+      error: errors[0].message,
+    };
+  }
+  return {
+    data: data.patchNPlaceDayNowCount.data,
+    message: "ok > PatchNPlaceDayNowCount ",
+    error: "",
+  };
+};
