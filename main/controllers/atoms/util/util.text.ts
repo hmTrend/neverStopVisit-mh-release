bb3ed51bed4f1d0b186d1f1e42e7c2ea164675a6
applepykim <applepykim@naver.com>
2025-02-26 22:43:39 +0900

    v5.116.6 로그인 풀림 개선

--- main/controllers/atoms/util/util.text.ts
+++ main/controllers/atoms/util/util.text.ts
@@ -2,6 +2,9 @@ export class UtilText {
   static errorMessageTrans({ errMessage }: { errMessage: string }) {
     console.log("errMessage 333322222111111");
     console.log(errMessage);
+    if (errMessage.includes("this is login button")) {
+      return "로그인 풀림";
+    }
     if (errMessage.includes("#_sr_lst_")) {
       return "상품 미발견";
     }
